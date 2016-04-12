68fe2e504fce0fbdf067cd247cf8456b1c1d67b0
Airwhale <scgeer@gm**l.c*m>
2016-04-11 22:22:17 -0700

    Minor change in Shaun's section notes

--- discussion/shaun/section2_4_11.R
+++ discussion/shaun/section2_4_11.R
@@ -14,7 +14,8 @@ summary(profiles$age)
 #4)What is the distribution of reported sexes?
 table(profiles$sex)
 # Make a figure describing sex in the dataset. Make it look pretty!
-dotchart(table(profiles$sex),main="Sexes in Okcupid data", xlab="number", ylab="ages",xlim=c(0,50000))
+dotchart(table(profiles$sex),main="Sexes in Okcupid data", 
+         xlab="number", ylab="ages",xlim=c(0,50000))
 
 #5) What about sexual orientation? Make a figure for this, also.  
 table(profiles$orientation)
@@ -30,7 +31,7 @@ females=subset(profiles,sex=="f")
 dotchart(table(females$orientation),main="Sexual orientation of women",
          xlab="frequency" , ylab="Orientation", mgp=c(3.7,0,1))
 dotchart(table(males$orientation), main="Sexual orientation of men",
-         xlab="frequency" , ylab="Orientation", mgp.axis=c(3.7,0,1))
+         xlab="frequency" , ylab="Orientation", mgp=c(3.7,0,1))
 
 #7)What other interesting plots/data can you pull from this dataset?  
 # You will have to be more creative for projects 3 and beyond, so look around the data and 
